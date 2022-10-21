dbd4134fcc64473934e70d37a85dd89003238863
ArinaGazeeva <86180876+ArinaGazeeva@users.noreply.github.com>
2022-10-21 16:11:41 +0500

    result_1

---
+++ result_1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+float result (int K, float resistance[]);
+
+float result (int K, float *resistance)
+{
+    float resistance_final = 0, deviation = 0;
+
+    for (int i = 0; i < K; i++)
+        resistance_final += resistance[i];
+
+    resistance_final /= K;
+
+    for (int i = 0; i < K; i++)
+        deviation += (resistance[i] - resistance_final)*(resistance[i] - resistance_final);
+
+    deviation = sqrt(deviation) / K;
+
+    printf("The desired resistance and standart deviation\n%.3lg +- %.3lg\n", resistance_final, deviation);
+
+    return resistance_final;
+}
