a28eebc381e92fe175014d74a9ce26bf2e4a19c1
VoyakaGOD <ab3000645@gmail.com>
2022-10-21 13:12:46 +0300

    some fixes, sorry(

--- check.c
+++ check.c
@@ -3,8 +3,7 @@
 int check(int K, float resistance[], float resistance_final)
 {
     float sum = 0;
-    int count = sizeof(resistance) / sizeof(float);
-    for(int i = 0; i < count; i++)
+    for(int i = 0; i < K; i++)
         sum += resistance[i] - resistance_final;
 
     return (-FLT_EPSILON < sum) && (sum < FLT_EPSILON);
--- lab.c
+++ lab.c
@@ -1,4 +1,5 @@
 #include "lab.h"
+#include <stdio.h>
 
 int main()
 {
